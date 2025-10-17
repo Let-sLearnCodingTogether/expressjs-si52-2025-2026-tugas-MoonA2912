6e182aa1e6ca2d2f7f10724f33042bf5ea48309c
MoonA2912 <monalisaeffendy_2327240014@mhs.mdp.ac.id>
2025-10-17 13:35:53 +0700

    Membuat user login

--- controller/authController.js
+++ controller/authController.js
@@ -1,6 +1,6 @@
 import UserModel from "../models/userModel.js";
 import { hash } from "../utils/hashUtil.js";
-
+import { compare } from "../utils/hashUtil.js";
 
 export const register = async (req, res) => {
     try{
@@ -27,4 +27,41 @@ export const register = async (req, res) => {
             data: null
         })
     }
+}
+
+export const login = async (req, res) => {
+    try {
+        const loginData = req.body
+
+        const user = await UserModel.findOne({
+            email: loginData.email
+        })
+
+        if (!user) {
+            return res.status(404).json({
+                message: "User tidak ditemukan",
+                data: null
+            })
+        }
+        if (compare(loginData.password, user.password)) {
+            res.status(200).json({
+                message: "Login Berhasil",
+                data: {
+                    username: user.username,
+                    email: user.email,
+                    token: "TOKEN"
+                }
+            })
+            return res.status(401).json({
+                message: "Login Tidak Berhasil",
+                data: null
+            })
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: error,
+            data: null
+        })
+    }
+
 }
--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,6 @@ import * as authController from "../controller/authController.js"
 const api= express.Router()
 
 api.post("/register", authController.register )
-
+api.post ('/login', authController.login)
 
 export default api
